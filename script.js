c3750b9fa5ed58921d684cfc3d2536ddd7674ff0
musaabuu <sgtmusabu@gmail.com>
2025-02-19 17:27:39 +0100

    Drag And Drop Feature :)

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const cards = document.querySelectorAll('.card');
+const container = document.querySelector('.container');
+const contained = document.querySelector('.contained');
 // console.log(cards);
 
-cards.forEach(card => {
-    card.addEventListener('dragstart', () => {
-        i = +this.closest('.div');
-        console.log(i);
+for (let card of cards) {
+    card.addEventListener('dragstart', (e) => {
+        let selected = e.target;
+        contained.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        });
+        contained.addEventListener('drop', (e) => {
+            contained.appendChild(selected);
+            selected = null;
+        });
+        container.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        });
+        container.addEventListener('drop', (e) => {
+            container.appendChild(selected);
+            selected = null;
+        });
     });
-    card.addEventListener('dragover', () => {
-    });
-    card.addEventListener('dragleave', () => {
-    });
-    card.addEventListener('drop', () => {
-    });
-
-    
-});
+}
